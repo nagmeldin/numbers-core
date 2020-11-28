5772b86f8bb356bd1f255d13e1412b9de328c870
nagmeldin <eldin.nagm@gmail.com>
2020-11-28 15:41:51 -0700

    Updated file

--- src/main/groovy/org/zone/Application.groovy
+++ src/main/groovy/org/zone/Application.groovy
@@ -3,6 +3,9 @@ package org.zone
 import io.micronaut.runtime.Micronaut
 import groovy.transform.CompileStatic
 
+//Author: Nagm eldin
+//Since: July 2002
+
 @CompileStatic
 class Application {
     static void main(String[] args) {
